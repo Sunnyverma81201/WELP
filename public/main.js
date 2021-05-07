92059c83d6046a658a2717d860c2f5c9351171e2
Apoorv Verma <40027838+Sunnyverma81201@users.noreply.github.com>
2021-05-07 13:51:17 +0530

    Started the work on form

---
+++ public/main.js
@@ -0,0 +1,14 @@
+      // Firebase initialization
+      const firebaseConfig = {
+        apiKey: "AIzaSyB8qG3xFLp4wWCd5cQU-po2DT6WNdy6bCE",
+        authDomain: "welp-bba1e.firebaseapp.com",
+        databaseURL: "https://welp-bba1e-default-rtdb.firebaseio.com",
+        projectId: "welp-bba1e",
+        storageBucket: "welp-bba1e.appspot.com",
+        messagingSenderId: "168708457434",
+        appId: "1:168708457434:web:fedd854e09d6525c0d36e3",
+        measurementId: "G-JSF4XKPM86",
+      };
+      firebase.initializeApp(firebaseConfig);
+      const db = firebase.firestore();
+      db.settings({timestampInSnapshots: true});

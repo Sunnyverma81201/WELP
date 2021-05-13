929677d7b539e8e7cc8be6f6f088d608d3c34b7e
Apoorv Verma <40027838+Sunnyverma81201@users.noreply.github.com>
2021-05-13 20:23:44 +0530

    cleared the project

--- public/js/main.js
+++ public/js/main.js
@@ -22,15 +22,15 @@ menuHamburger.addEventListener('click',() => {
 });
 
 // Firebase initialization
-const fire = firebase.initalizeApp(firebaseConfig);
-const firebaseConfig = {
-  apiKey: "AIzaSyB8qG3xFLp4wWCd5cQU-po2DT6WNdy6bCE",
-  authDomain: "welp-bba1e.firebaseapp.com",
-  databaseURL: "https://welp-bba1e-default-rtdb.firebaseio.com",
-  projectId: "welp-bba1e",
-  storageBucket: "welp-bba1e.appspot.com",
-  messagingSenderId: "168708457434",
-  appId: "1:168708457434:web:fedd854e09d6525c0d36e3",
-  measurementId: "G-JSF4XKPM86",
-};
-const db = fire.firestore();
+// const fire = firebase.initalizeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB8qG3xFLp4wWCd5cQU-po2DT6WNdy6bCE",
+//   authDomain: "welp-bba1e.firebaseapp.com",
+//   databaseURL: "https://welp-bba1e-default-rtdb.firebaseio.com",
+//   projectId: "welp-bba1e",
+//   storageBucket: "welp-bba1e.appspot.com",
+//   messagingSenderId: "168708457434",
+//   appId: "1:168708457434:web:fedd854e09d6525c0d36e3",
+//   measurementId: "G-JSF4XKPM86",
+// };
+// const db = fire.firestore();

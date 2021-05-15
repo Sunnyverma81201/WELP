f9e12c19bcc3a0b32c7f595d95f8c9aaec5bda4a
Apoorv Verma <40027838+Sunnyverma81201@users.noreply.github.com>
2021-05-15 22:08:06 +0530

    stability improvement and UI refining

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 var navbar = document.getElementById("navbar");
 var menuHamburger= document.getElementById("ham");
+var navigationButton = document.getElementsByClassName("nav_button");
 var sticky = navbar.offsetTop;
 
 // code for sticky navigation bar
@@ -21,16 +22,22 @@ menuHamburger.addEventListener('click',() => {
   }
 });
 
+// to close the nav bar as soon as any of the button is tapped/Clicked
+
+navigationButton.addEventListener('click',() => {
+  menuHamburger.click;
+});
+
 // Firebase initialization
-// const fire = firebase.initalizeApp(firebaseConfig);
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB8qG3xFLp4wWCd5cQU-po2DT6WNdy6bCE",
-//   authDomain: "welp-bba1e.firebaseapp.com",
-//   databaseURL: "https://welp-bba1e-default-rtdb.firebaseio.com",
-//   projectId: "welp-bba1e",
-//   storageBucket: "welp-bba1e.appspot.com",
-//   messagingSenderId: "168708457434",
-//   appId: "1:168708457434:web:fedd854e09d6525c0d36e3",
-//   measurementId: "G-JSF4XKPM86",
-// };
-// const db = fire.firestore();
+const fire = firebase.initalizeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: "AIzaSyB8qG3xFLp4wWCd5cQU-po2DT6WNdy6bCE",
+  authDomain: "welp-bba1e.firebaseapp.com",
+  databaseURL: "https://welp-bba1e-default-rtdb.firebaseio.com",
+  projectId: "welp-bba1e",
+  storageBucket: "welp-bba1e.appspot.com",
+  messagingSenderId: "168708457434",
+  appId: "1:168708457434:web:fedd854e09d6525c0d36e3",
+  measurementId: "G-JSF4XKPM86",
+};
+const db = fire.firestore();

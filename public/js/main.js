001d1fa74ec604e2580e4863dc6eca79c983fdd3
Apoorv Verma <40027838+Sunnyverma81201@users.noreply.github.com>
2021-05-16 10:15:18 +0530

    added country an city data

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import { getFirestore } from 'firebase/firestore';
-// const firebaseApp = initializeApp({
-//   apiKey: "AIzaSyB8qG3xFLp4wWCd5cQU-po2DT6WNdy6bCE",
-//   authDomain: "welp-bba1e.firebaseapp.com",
-//   databaseURL: "https://welp-bba1e-default-rtdb.firebaseio.com",
-//   projectId: "welp-bba1e",
-//   storageBucket: "welp-bba1e.appspot.com",
-//   messagingSenderId: "168708457434",
-//   appId: "1:168708457434:web:fedd854e09d6525c0d36e3",
-//   measurementId: "G-JSF4XKPM86",
-// });
 var navbar = document.getElementById("navbar");
 var menuHamburger= document.getElementById("ham");
 var navigationButton = document.getElementsByClassName("nav_button");
